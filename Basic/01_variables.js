54539e5af008e3e2b69ff01cc61560df0ab61050
Ganesh <146371917+Ganesh33333@users.noreply.github.com>
2024-01-02 10:33:22 +0000

    variables

--- Basic/01_variables.js
+++ Basic/01_variables.js
@@ -3,4 +3,9 @@ let accountEmail="abc@123"
 var accountPass="qwer4"
 accountCity= "belgaon"
 
+// accountId=12
+accountEmail="qw@we.com"
+accountPass="skh"
+accountCity="singhapur"
+
 console.table([accountId,accountEmail,accountPass,accountCity]);
