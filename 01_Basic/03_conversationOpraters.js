7dbba998e628b180faae7cbdba416ff93dac3ba1
Ganesh <146371917+Ganesh33333@users.noreply.github.com>
2024-03-06 06:57:49 +0000

    learen opraters

---
+++ 01_Basic/03_conversationOpraters.js
@@ -0,0 +1,23 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log("1" + 2/2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3+4)*5%3);
+// console.log(((3+4)*5)%3);
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+// equality chaeck
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null<=0);
+// console.log(null>=0);
+
+// ===
+
+console.log("2" === 2);
